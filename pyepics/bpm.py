7d143bae8f321e4f286bd0df7940db0155e13869
stevemolloy <sdmolloy@gmail.com>
2017-11-28 10:44:05 +0100

    Basic aquisition tested and working

--- pyepics/bpm.py
+++ pyepics/bpm.py
@@ -3,13 +3,13 @@ import os
 
 PVROOT = "BPM:"
 
-def triggerAquisition():
-    aquisitionPV = PV(f'{PVROOT}Acquire')
-    aquisitionPV.value = 1
-
 os.environ['EPICS_CA_ADDR_LIST'] = '10.0.16.173'
 os.environ['EPICS_CA_AUTO_ADDR_LIST'] = 'NO'
 os.environ['EPICS_CA_MAX_ARRAY_BYTES'] = '200000000'
+
+def triggerAquisition():
+    aquisitionPV = PV(f'{PVROOT}Acquire')
+    aquisitionPV.value = 1
 
 class BPMdata:
     def __init__(self, BPMnum):
--- pyepics/testing/TestBpm.py
+++ pyepics/testing/TestBpm.py
@@ -1,4 +1,9 @@
 import unittest
+from ..bpm import BPMdata, triggerAquisition
 
 
-
+class TestBPMdata(unittest.TestCase):
+    def test_aquisition_works(self):
+        triggerAquisition()
+        bpm = BPMdata(1)
+        self.assertGreater(bpm.numSampPointsPV.get(), 0)
