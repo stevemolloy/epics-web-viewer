3711a23f0875305e9e5833ae591e3913fec0af16
stevemolloy <sdmolloy@gmail.com>
2017-12-01 00:07:31 +0100

    SQLalchemy demo working

--- pyepics/database_connection.py
+++ pyepics/database_connection.py
@@ -1,19 +1,37 @@
-import sqlalchemy
 import os
+from sqlalchemy import ARRAY as Array, Float, Column, Integer, DateTime, create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.sql import func
+from time import sleep
+from pyepics.bpm import BPMdata
+
+user = os.getenv('POSTGRESUSER')
+passwd = os.getenv('POSTGRESPASS')
+dbname = os.getenv('POSTGRESDBNAME')
+host = '0.0.0.0'
+port = '5432'
+engine = create_engine(f'postgresql://{user}:{passwd}@{host}:{port}/{dbname}', echo=True)
+
+Session = sessionmaker(bind=engine)
+session = Session()
+Base = declarative_base()
 
 
-def connect(user, password, db, host='localhost', port=5432):
-    url = 'postgresql://{}:{}@{}:{}/{}'
-    url = url.format(user, password, host, port, db)
-    con = sqlalchemy.create_engine(url, client_encoding='utf8')
-    meta = sqlalchemy.MetaData(bind=con, reflect=True)
-    return con, meta
+class BPMSumSignal(Base):
+    __tablename__ = 'bpm_sum_signal'
+    id = Column(Integer, primary_key=True)
+    signal = Column(Array(Float))
+    time_created = Column(DateTime(timezone=True), server_default=func.now())
+    time_updated = Column(DateTime(timezone=True), onupdate=func.now())
 
 
 if __name__ == "__main__":
-    user = os.getenv('POSTGRESUSER')
-    passwd = os.getenv('POSTGRESPASS')
-    dbname = os.getenv('POSTGRESDBNAME')
-    connex, metadata = connect(user=user, password=passwd, db=dbname, host='0.0.0.0')
-    print(connex)
-    print(metadata)
+    bpm = BPMdata(1)
+    while True:
+        try:
+            session.add(BPMSumSignal(signal=[float(a) for a in bpm.sumSigAmp()]))
+            session.commit()
+        except TypeError:
+            pass
+        sleep(5)
