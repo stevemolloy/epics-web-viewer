f5f8e5239d2a377e08394aa944e4598ff74ba477
stevemolloy <sdmolloy@gmail.com>
2017-12-10 22:53:49 +0100

    docker-compose creates a postgres db ready for database_connection.py

--- pyepicsContainer/pyepics/database_connection.py
+++ pyepicsContainer/pyepics/database_connection.py
@@ -1,19 +1,22 @@
 import os
 from sqlalchemy import ARRAY, Float, Column, Integer, DateTime, create_engine
-from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.sql import func
+from sqlalchemy.util.queue import Empty
 from time import sleep
-import threading
 
 Base = declarative_base()
 
 user = os.getenv('POSTGRES_USER')
 passwd = os.getenv('POSTGRES_PASSWORD')
 dbname = os.getenv('POSTGRES_DB')
-host = '0.0.0.0'
+# host = '0.0.0.0'
+host = 'postgres'
 port = '5432'
-engine = create_engine(f'postgresql://{user}:{passwd}@{host}:{port}/{dbname}', echo=True)
+engine = create_engine(
+        f'postgresql://{user}:{passwd}@{host}:{port}/{dbname}',
+        echo=True
+    )
 
 
 class BPMSumAmplitude(Base):
@@ -32,34 +35,10 @@ class BPMSumPhase(Base):
     time_updated = Column(DateTime(timezone=True), onupdate=func.now())
 
 
-Base.metadata.create_all(engine)
-
-
-class LockedDBUpdate:
-    def __init__(self, bpmnum):
-        self.lock = threading.Lock()
-
-        db_session = sessionmaker(bind=engine)
-        self.session = db_session()
-
-        self.bpm = bpmnum
-
-    def get_and_commit(self):
-        with self.lock:
-            try:
-                self.session.add_all([
-                    BPMSumAmplitude(signal=[float(a) for a in self.bpm.sumSigAmp()]),
-                    BPMSumPhase(signal=[float(a) for a in self.bpm.sumSigPhase()]),
-                ])
-                self.session.commit()
-            except TypeError:
-                pass
-
-
-if __name__ == "__main__":
-    from bpm import BPMdata
-    bpm = BPMdata(1)
-    ldb = LockedDBUpdate(bpm)
-    while True:
-        ldb.get_and_commit()
-        sleep(5)
+while True:
+    try:
+        print(engine)
+        Base.metadata.create_all(engine)
+        break
+    except Empty:
+        sleep(2)
